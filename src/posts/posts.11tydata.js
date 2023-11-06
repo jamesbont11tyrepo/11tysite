ef2ad9bd3f643cbe72d1de709e0f71d6be8e09ff
jamesbont11tyrepo <HomeFolder@Macintosh.local>
2023-11-06 08:16:25 -0800

    updated config and index, also added posts data config

---
+++ src/posts/posts.11tydata.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+  eleventyComputed: {
+    permalink: (data) => data.page.filePathStem.slice(6) + "/", // take the stem (no extension), lop off the `posts/` at the start, and add a `/`
+  }
+};
