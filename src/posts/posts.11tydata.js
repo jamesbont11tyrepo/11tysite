fca858c9f80cdba20c40c66f925f62934763d798
jamesbont11tyrepo <149445310+jamesbont11tyrepo@users.noreply.github.com>
2023-11-07 13:20:51 -0800

    Revert "Remove `/posts` from permalinks"

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,20 +1,5 @@
-const POST_FILE_NAME_RE = /^(?:\/posts)?(\/.+)/;
-
 module.exports = () => ({
   eleventyComputed: {
-    permalink: (data) => {
-      if (data.permalink !== undefined && data.permalink !== "") {
-        return data.permalink;
-      }
-
-      const { filePathStem } = data.page;
-
-      const matches = POST_FILE_NAME_RE.exec(filePathStem);
-      if (matches === null) {
-        return data.permalink;
-      }
-
-      return matches[1] + "/";
-    }, // take the stem (no extension), lop off the `posts/` at the start, and add a `/`
-  },
+    permalink: (data) => data.page.filePathStem.slice(6) + "/", // take the stem (no extension), lop off the `posts/` at the start, and add a `/`
+  }
 });
